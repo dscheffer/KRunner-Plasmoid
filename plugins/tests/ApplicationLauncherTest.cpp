9b520a3c3d126e2fbbb4cbcae85736c8433ec294
Dennis Scheffer <dennis.scheffer1993@gmail.com>
2018-04-27 14:27:29 +0200

    Fix license header in ApplicationLauncherTest

--- plugins/tests/ApplicationLauncherTest.cpp
+++ plugins/tests/ApplicationLauncherTest.cpp
@@ -1,20 +1,20 @@
 /*
  *   Copyright (c) 2018 Dennis Scheffer.
  *
- *   This file is part of Minimal Menu.
+ *   This file is part of KRunner Plasmoid.
  *
- *   Minimal Menu is free software: you can redistribute it and/or modify
+ *   KRunner Plasmoid is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   Minimal Menu is distributed in the hope that it will be useful,
+ *   KRunner Plasmoid is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with Minimal Menu.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with KRunner Plasmoid.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "gtest/gtest.h"
